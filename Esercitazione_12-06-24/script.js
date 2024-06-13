e5b335d142e6f690ea31ca53f8acaa880d33693d
Nicola Marmugi <marmu1@hotmail.it>
2024-06-13 02:19:52 +0200

    feat: esercitazione 12-06-24

--- Esercitazione_12-06-24/script.js
+++ Esercitazione_12-06-24/script.js
@@ -5,55 +5,83 @@ const inputCategoryEl = document.querySelector('.category-id');
 const inputImagesEl = document.querySelector('.images');
 const buttonSendEl = document.querySelector('.button-send');
 const buttonDeleteEl = document.querySelector('.button-delete');
+const inputDeleteIdEl = document.querySelector('.delete-id');
+const buttonResetEl = document.querySelector('.button-reset');
+const modal = document.getElementById("deleteModal");
+const span = document.getElementsByClassName("close")[0];
 
 const BASE_URL = "https://api.escuelajs.co/";
 const endpoint = "api/v1/products";
 const url = BASE_URL + endpoint;
 
-
-async function POST(newObj) {
-	const response = await fetch(url, {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(newObj),
-	})
-	const data = await response.json();
-	console.log(data.id);
-	return data.id; 
+const options = {
+	headers: {
+		accept: 'application/json'
+	}
 }
 
-buttonSendEl.addEventListener('click', (e) => {
+const GET = async () => {
+	const response = await fetch(url, options);
+	const data = await response.json();
+	console.log(data);
+}
+
+GET();
+
+const POST = async (obj) => {
+	const response = await fetch(url, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(obj)
+	});
+	const data = await response.json();
+	return data.id;
+}
+
+buttonSendEl.addEventListener('click', async (e) => {
 	e.preventDefault();
-	const objForm = {
+	const newObj = {
 		title: inputTitleEl.value,
 		price: inputPriceEl.value,
 		description: inputDescriptionEl.value,
 		categoryId: inputCategoryEl.value,
 		images: [inputImagesEl.value]
 	}
-	POST(objForm);
+	console.log('Oggetto creato con ID:', await POST(newObj), newObj);
 })
 
-const DELETE = async (productId) => {
-	const deleteUrl = `${BASE_URL}${endpoint}/${productId}`;
-	const response = await fetch(deleteUrl, {
-		method: "DELETE",
+const DELETE = async (id) => {
+	const response = await fetch(`${url}/${id}`, {
+		method: 'DELETE',
 		headers: {
-			"Content-Type": "application/json",
+			'Content-Type': 'application/json'
 		}
-	})
+	});
+	const data = await response.json();
+	modal.style.display = "block";
+	setTimeout(() => {
+		modal.style.display = "none";
+	}, 5000);
+	return data;
 }
 
-buttonDeleteEl.addEventListener('click', async (e) => {
+buttonDeleteEl.addEventListener('click', (e) => {
 	e.preventDefault();
-	const productId = prompt("Inserisci l'ID del prodotto da eliminare:");
-	if (!productId || isNaN(productId)) {
-		alert("ID del prodotto non valido.");
-		console.log('ID non valido')
-		return;
-	}
-	console.log('ID eliminato')
-	await DELETE(productId);
+	DELETE(inputDeleteIdEl.value);
+	console.log(`Oggetto con ID: ${inputDeleteIdEl.value} eliminato`);
 })
+
+buttonResetEl.addEventListener('click', () => {
+	inputTitleEl.value = '';
+	inputPriceEl.value = '';
+	inputDescriptionEl.value = '';
+	inputCategoryEl.value = '';
+	inputImagesEl.value = '';
+	inputDeleteIdEl.value = '';
+})
+
+span.addEventListener('click', () => {
+	modal.style.display = "none";
+});
