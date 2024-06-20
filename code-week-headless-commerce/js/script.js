dd224f3fd136914d5f1cffb5988616dab9fc83c1
Nicola Marmugi <marmu1@hotmail.it>
2024-06-20 12:24:06 +0200

    feat: aggiunta traduzione in inglese homepage

--- code-week-headless-commerce/js/script.js
+++ code-week-headless-commerce/js/script.js
@@ -2,4 +2,43 @@ import { MODAL } from "./modals.js";
 import { HOME } from "./home.js";
 
 HOME();	//RICHIAMO HOME DA HOME.JS
-MODAL(); //RICHIAMO MODAL DA COMPONENTS.JS
+MODAL(); //RICHIAMO MODAL DA COMPONENTS.JSz
+
+function loadLanguage(language) {
+	fetch(`./translations_${language}.json`).then(response => {
+		return response.json();
+	}).then(translations => {
+		applyTranslations(translations);
+	}).catch(error => {
+		console.error(error);
+	})
+}
+
+function applyTranslations(translations) {
+	for (const key in translations) {
+		if (translations.hasOwnProperty(key)) {
+			const element = document.getElementById(key);
+			if (element) {
+				if (element.tagName === 'INPUT') {
+					element.placeholder = translations[key];
+				} else {
+					element.textContent = translations[key];
+				}
+			}
+		}
+	}
+}
+
+const it = document.querySelectorAll('.btn-italian');
+it.forEach(button => {
+	button.addEventListener('click', function() {		
+		loadLanguage('it');
+	})
+})
+
+const en = document.querySelectorAll('.btn-english');
+en.forEach(button => {
+	button.addEventListener('click', function() {
+		loadLanguage('en');
+	})
+})
