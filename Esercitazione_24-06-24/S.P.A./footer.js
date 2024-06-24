c61d4085dc7dbdf510cbb75444c57fbdebb103cc
Nicola Marmugi <marmu1@hotmail.it>
2024-06-25 00:50:12 +0200

    feat: modifiche allo script del footer esercitazione  24-06-24

--- Esercitazione_24-06-24/S.P.A./footer.js
+++ Esercitazione_24-06-24/S.P.A./footer.js
@@ -1,50 +1,62 @@
 function createFooter() {
 	const menuHTML = getMenuHTML();
 	return `
-	<footer class="footer">
-		<div class="logo logoFooter">Logo</div>
-		<nav class="nav">
-			${menuHTML}
-		</nav>
+	<footer>
+		<div class="footer-logo">LOGO</div>
+		${menuHTML}
 	</footer>
 	`;
 }
+
+//${menuHTML}
 
 function getMenuHTML() {
 	const menuItems = [
 	{
 		label: "About us",
 		children: [
-			{ label: "Company info" },
-			{ label: "Careers" },
-			{ label: "Altre cose" },
+			{ label: "Company info", href: "#" },
+			{ label: "Careers", href: "#" },
+			{ label: "Altre cose", href: "#" },
 		],
 	},
 	{
 		label: "About us",
 		children: [
-			{ label: "Company info" },
-			{ label: "Careers" },
-			{ label: "Altre cose" },
+			{ label: "Company info", href: "#" },
+			{ label: "Careers", href: "#" },
+			{ label: "Altre cose", href: "#" },
 		],
 	},
+	{
+		label: "About us",
+		children: [
+			{ label: "Company info", href: "#" },
+			{ label: "Careers", href: "#" },
+			{ label: "Altre cose belle", href: "#" },
+		],
+	}
 	];
+	return createMenu(menuItems);
+}
+
+function createMenu(options = []) {
+	const menu = options.map(menuItems => `<li class="menu">${menuItems.label}
+			${createSubMenu(menuItems.children)}
+		</li>`).join('');
 	return `
-	<ul class="menu">
-		${menuItems.map((item) => getMenuItemHTML(item)).join("")}
+	<ul class="main-menu">
+		${menu}
 	</ul>
 	`;
 }
 
-function getMenuItemHTML(options) {
-	const { label, href = "#", children = [] } = options;
-	const subitemsHTML = children.map((item) => `<li class="item subitem">${item.label}</li>`).join("");
-	const subMenuHTML = "<ul>" + subitemsHTML + "</ul>";
+function createSubMenu(options = []) {
+	const children = options.map(children => `<li class="subMenu"><a href="${children.href}">${children.label}</a></li>`).join('');
 	return `
-	<li class="item">
-		<a href="${href}">${label}</a>
-		${children.length > 0 ? subMenuHTML : ""}
-	</li>
+	<ul>
+		${children}
+	</ul>
 	`;
 }
 
